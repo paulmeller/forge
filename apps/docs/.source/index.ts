9931cec807e3cb3f94edc27bac1ce0e8ccae3686
Paul Meller <14568+paulmeller@users.noreply.github.com>
2026-04-28 23:41:04 +1000

    chore: exclude generated .source directory from version control

--- apps/docs/.source/index.ts
+++
@@ -1,5 +0,0 @@
-// @ts-nocheck -- skip type checking
-import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1777383037429"
-import { _runtime } from "fumadocs-mdx"
-import * as _source from "../source.config"
-export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"/Users/paulmeller/Projects/forge/apps/docs/content/docs/index.mdx"}, data: docs_0 }], [{"info":{"path":"meta.json","absolutePath":"/Users/paulmeller/Projects/forge/apps/docs/content/docs/meta.json"},"data":{"title":"Docs","pages":["index"]}}])
